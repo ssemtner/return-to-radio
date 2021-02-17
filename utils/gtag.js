71f62a99ac74ef45e86d4638ed94adda89d9f57a
Scott <sjsemtner@gmail.com>
2021-02-16 23:06:57 -0800

    fixed error in gtag

--- utils/gtag.js
+++ utils/gtag.js
@@ -1,21 +1,14 @@
 export const GA_TRACKING_ID = "G-CW4GVYFL22";
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
-export const pageview = (url: URL) => {
+export const pageview = (url) => {
   window.gtag("config", GA_TRACKING_ID, {
     page_path: url
   });
 };
 
-type GTagEvent = {
-  action: string;
-  category: string;
-  label: string;
-  value: number;
-};
-
 // https://developers.google.com/analytics/devguides/collection/gtagjs/events
-export const event = ({ action, category, label, value }: GTagEvent) => {
+export const event = ({ action, category, label, value }) => {
   window.gtag("event", action, {
     event_category: category,
     event_label: label,
