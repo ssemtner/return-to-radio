6b6ba7d13ff178e50ac3abe910e646524c8fd40b
Scott Semtner <sjsemtner@gmail.com>
2021-03-02 12:24:35 -0800

    added header

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,28 @@
 import Image from 'next/image'
 import React from 'react'
+import { Typography } from 'antd'
+
+const {Title} = Typography
 
 export default function Home() {
     return (
-        <img
-            src='/poster.png'
-            style={{
-                maxWidth: '90%',
-                maxHeight: '90%',
-                bottom: '0',
-                left: '0',
-                margin: 'auto',
-                overflow: 'auto',
-                position: 'fixed',
-                right: '0',
-                top: '0',
-                objectFit: 'contain',
-            }}
-        />
+        <>
+            <Title>More Information Coming Soon!</Title>
+            <img
+                src='/poster.png'
+                style={{
+                    maxWidth: '90%',
+                    maxHeight: '90%',
+                    bottom: '0',
+                    left: '0',
+                    margin: 'auto',
+                    overflow: 'auto',
+                    position: 'fixed',
+                    right: '0',
+                    top: '0',
+                    objectFit: 'contain',
+                }}
+            />
+        </>
     )
 }
